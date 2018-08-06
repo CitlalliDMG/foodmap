fb710919f6ccd45d2e74ced7036e94e24a8970c1
Citlalli DMG <citlallidmg@gmail.com>
2018-08-06 08:14:09 -0500

    update folder structure and upload splash screen

---
+++ src/js/splash.js
@@ -0,0 +1,18 @@
+const init = () => {
+  const preload = document.getElementById('preload');
+  let loading = 0;
+  const frame = () => {
+    if (loading == 100) {
+      clearInterval(id);
+      window.location.assign('../src/views/home.html');
+    } else {
+      loading = loading + 1;
+      if (loading == 90) {
+        preload.style.animation = 'fadeout 2s ease';
+      }
+    };
+  };
+  let id = setInterval(frame, 30);
+};
+
+window.onload = init();
